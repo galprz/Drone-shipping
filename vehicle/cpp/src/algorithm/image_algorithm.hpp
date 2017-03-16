9d22708c054d7723eb08b1dc68f0a00dfc5b2cf6
gal peretz <galperetz@galps-mbp.mynet>
2017-03-17 01:08:39 +0200

    adding doc to the algorithem module

--- vehicle/cpp/src/algorithm/image_algorithm.hpp
+++ vehicle/cpp/src/algorithm/image_algorithm.hpp
@@ -26,12 +26,28 @@ using namespace cv;
 #define THRESHOLD_RADIUS_MIN 5
 #define THRESHOLD_RADIUS_MAX 10000000
 #define MIN_CENTERS 3
-
+/**
+ * we provide two built in image algorithms . you can add you own here
+ *
+ */
 namespace VehicleModule {
     namespace Algorithm {
-  
-        
+
+        /**
+         * finds a bullseye target's center only if all the target in the frame
+         * that means the target's center must be inside the frame unlike 'find_bullseye_direction'
+         * @param  img  input image
+         * @param  out  center of target in pixels if we found one
+         * @return true if found target else false
+         */
         bool find_bullseye(const Mat& img, Point& out);
+        /**
+         * finds a bullseye target's center even if we have just part of the target in the frame
+         * that means the target's center could be outside the frame
+         * @param  img  input image
+         * @param  out  center of target in pixels if we found one
+         * @return true if found target else false
+         */
         bool find_bullseye_direction(const Mat& img, Point& out);
     }
 }
